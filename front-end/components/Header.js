dde4129346c07ef913f9950fa45053cd636e5740
walterthesmart <nwaugowalter@gmail.com>
2024-09-21 16:52:08 +0100

    Refactor hardhat.config.js to update Arbitrum RPC URL

--- front-end/components/Header.js
+++ front-end/components/Header.js
@@ -1,11 +1,15 @@
+import Link from "next/link"
 import { ConnectButton } from "web3uikit"
 
 export default function Header() {
+
     return (
         <nav className="p-5 border-b-2 flex flex-row">
-            <h1 className="py-4 px-4 font-bold text-3xl"> Decentralized Lottery Alx</h1>
+            <h1 className="py-4 px-4 font-bold text-3xl">Decentralized Lottery Alx</h1>
             <div className="ml-auto py-2 px-4">
+                <Link href='/LotteryEntrance'>
                 <ConnectButton moralisAuth={false}/>
+                </Link>
             </div>
         </nav>
     )
